4d508f0a4a8b132fbaffe5c5d39177a6675b465d
Jean Janrley <jeanjanrley@outlook.com>
2022-06-30 10:04:43 -0300

    Criação de regras de lint

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View>
+	return (
+		<View>
 
-    </View>
-  );
+		</View>
+	);
 }
